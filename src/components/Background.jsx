1dac71a26b927af4151f177567df3dc92ac4e1b5
Wiran_Larbi <71338128+Wiran-Larbi@users.noreply.github.com>
2023-08-19 14:40:28 +0100

    commited different bg

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,13 +1,15 @@
-import backgroundDesktop from '../gif/background7.gif'
+import backgroundDesktop from '../gif/wave.svg'
 
 export function BackgroundDesktop() {
 
 
     return <>
-        <div className="w-full absolute -z-10 top-2">
+        <div className="w-full absolute -z-10 top-2 h-52">
             <img src={backgroundDesktop}
                 alt="background image"
                 className="min-w-full h-auto object-cover" />
+
+               
         </div>
     </>
 }
